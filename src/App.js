f9eb2708c0006053ab6a158d1126aaeb2dc579ad
Raul Juliao <raulj@uninorte.edu.co>
2023-09-21 18:34:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button';
-import { useState } from 'react';
+import LikeBox from './components/LikeBox';
 
 function App() {
 
-    const [counter, setCounter] = useState(0)
-    const [decrement, setDecrement]  = useState(0)
-    const [disabled, setDisabled] = useState(true)
-
-    function handleLike(){
-        setCounter(counter + 1)
-    }
-
-    function handleUnlike(){
-      if(counter === 0){
-        setDisabled(false)
-      }else{
-        setDecrement(decrement - 1)
-      }
-    }
+    
 
     return (
       <div className="App">
         <header className="App-header">
           <h1>Increment button</h1>
-          <br/>
-          <Button  counter={counter} onClick={handleLike}/>
-
-          <Button isDisabled={disabled} counter={decrement} onClick={handleUnlike}/>
+            <LikeBox/>
         </header>
       </div>
     );
