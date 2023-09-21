23ad6dbdd660dd517c6b6f7363c83d8d326f4ba3
Raul Juliao <raulj@uninorte.edu.co>
2023-09-21 18:35:33 -0500

    Button modification

--- src/components/ButtonUi.js
+++ src/components/ButtonUi.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Button } from "@mui/material";
 
-function ButtonUi({isDisabled, counter, onClick}){
+function ButtonUi({isDisabled, counter, onClick, innerText}){
 
     return(
         <>
             <Button variant='contained' disabled={isDisabled} onClick={onClick}>
-                Like {counter}
+                {innerText} {counter}
             </Button>
         </>
     )
