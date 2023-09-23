929f6d4e16d09dfaaa172c8bf6fb7c16b50882f9
Raul Juliao <raulj@uninorte.edu.co>
2023-09-23 15:45:31 -0500

    json values

---
+++ src/assets/values.js
@@ -0,0 +1,8 @@
+const songs = [
+    { title: 'Anastasia', author: 'Unknown', isSymphony: false, id: 1 },
+    { title: 'Symphony No 9', author: 'Beethoven', isSymphony: true, id: 2 },
+    { title: 'Judas', author: 'Lady Gaga', isSymphony: true, id: 3 },
+    { title: 'La Vida es un Carnaval', author: 'Celia Cruz', isSymphony: false, id: 4 },
+    { title: 'Wattermelon Sugar', author: 'Harry Sytles', isSymphony: false, id: 5 },
+    { title: 'everything i wanted', author: 'Billie Eilish', isSymphony: true, id: 6 },
+  ];
